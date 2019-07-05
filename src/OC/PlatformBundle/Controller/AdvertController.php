b049807499f8f7b467163785120ab1649335358b
SergeB <sergebasset@gmail.com>
2019-07-05 13:32:10 +0200

    Add simple actions for the new routes

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -2,6 +2,7 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller{
     public function indexAction() {
@@ -10,4 +11,17 @@ class AdvertController extends Controller{
             ['name' => 'Serge']
         );
     }
+
+    public function viewAction($id) {
+        return new Response("Affichage de l'annonce d'id : ".$id);
+    }
+
+    public function slugAction($year, $slug, $_format) {
+        return new Response('On affiche l\'annonce du slug "'.$slug.'", créé en "'.$year.'" et au format "'.$_format.'"');
+    }
+
+    public function addAction() {
+        return new Response("Ajout (démerde toi avec ca !)");
+    }
+
 }
